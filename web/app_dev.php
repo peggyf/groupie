ab8414587f7ebc70780acd36d25eec71d6f56824
Développement d'application PHP <admdev-php@dev-web-test.pj.univ-amu.fr>
2014-11-18 11:22:19 +0100

    suppression index.php et ajout adresse VPN

--- web/app_dev.php
+++ web/app_dev.php
@@ -14,6 +14,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array(
 		'127.0.0.1', 
 		'10.193.1.1', 
+                '147.94.140.8',
 		'fe80::1', 
 		'::1'))
 ) {
