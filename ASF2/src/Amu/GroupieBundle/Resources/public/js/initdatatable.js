4a91a8c8368570ed6a9cae29cf6058ba6394bceb
peggy_fernandez <peggy.fernandez-blanco@univ-amu.fr>
2016-04-18 11:38:32 +0200

    Ajout fonction d'initialisation pour les datatables

---
+++ ASF2/src/Amu/GroupieBundle/Resources/public/js/initdatatable.js
@@ -0,0 +1,45 @@
+/**
+ * Created by peggy_fernandez on 18/04/2016.
+ */
+/*
+ The MIT License
+
+ Copyright (c) 2010 - Nicolas Martin <joliclic@gmail.com>
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+function initdatatable(elt) {
+    $(elt).DataTable({
+        "language": {
+            "lengthMenu": "Afficher _MENU_ éléments",
+            "zeroRecords": "Aucune entrée trouvée",
+            "info": "Affichage éléments _START_ à _END_ sur _TOTAL_ éléments",
+            "infoEmpty": "Pas d'entrée disponible",
+            "search":         "Recherche ",
+            "paginate": {
+                "first":      "Premier",
+                "last":       "Dernier",
+                "next":       "Suivant",
+                "previous":   "Précédent"
+            },
+        }
+    });
+}
+
