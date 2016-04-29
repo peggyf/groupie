8434539a72ad573754b80f16d0f0b684b1be28b6
peggy_fernandez <peggy.fernandez-blanco@univ-amu.fr>
2016-04-29 16:34:46 +0200

    Ajout fonctionnalité de tri datatable sur la colonne Admin

--- ASF2/src/Amu/GroupieBundle/Resources/public/js/initdatatable.js
+++ ASF2/src/Amu/GroupieBundle/Resources/public/js/initdatatable.js
@@ -1,29 +1,6 @@
 /**
  * Created by peggy_fernandez on 18/04/2016.
  */
-/*
- The MIT License
-
- Copyright (c) 2010 - Nicolas Martin <joliclic@gmail.com>
-
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- */
 
 function initdatatable(elt) {
     $(elt).DataTable({
@@ -45,8 +22,101 @@ function initdatatable(elt) {
                 "last":       "Dernier",
                 "next":       "Suivant",
                 "previous":   "Précédent"
-            },
+            }
         }
-    });
-}
+    })
+};
 
+function initdatatable_groupupdate(elt) {
+    $(elt).DataTable({
+        "language": {
+            "lengthMenu": 'Afficher <select>'+
+            '<option value="10">10</option>'+
+            '<option value="25">25</option>'+
+            '<option value="50">50</option>'+
+            '<option value="100">100</option>'+
+            '<option value="-1">Tous</option>'+
+            '</select> éléments',
+            "zeroRecords": "Aucune entrée trouvée",
+            "info": "Affichage éléments _START_ à _END_ sur _TOTAL_ éléments",
+            "infoEmpty": "Pas d'élément disponible",
+            "search":         "Recherche ",
+            "infoFiltered":   "(filtrage sur un total de _MAX_ éléments)",
+            "paginate": {
+                "first":      "Premier",
+                "last":       "Dernier",
+                "next":       "Suivant",
+                "previous":   "Précédent"
+            }
+        },
+        "columns": [
+            null, null,null, null,
+            { "orderDataType": "dom-checkbox" },
+            { "orderDataType": "dom-checkbox" }
+        ]
+
+    })
+};
+
+
+function initdatatable_userupdate(elt) {
+    $(elt).DataTable({
+        "language": {
+            "lengthMenu": 'Afficher <select>'+
+            '<option value="10">10</option>'+
+            '<option value="25">25</option>'+
+            '<option value="50">50</option>'+
+            '<option value="100">100</option>'+
+            '<option value="-1">Tous</option>'+
+            '</select> éléments',
+            "zeroRecords": "Aucune entrée trouvée",
+            "info": "Affichage éléments _START_ à _END_ sur _TOTAL_ éléments",
+            "infoEmpty": "Pas d'élément disponible",
+            "search":         "Recherche ",
+            "infoFiltered":   "(filtrage sur un total de _MAX_ éléments)",
+            "paginate": {
+                "first":      "Premier",
+                "last":       "Dernier",
+                "next":       "Suivant",
+                "previous":   "Précédent"
+            }
+        },
+        "columns": [
+            null,
+            { "orderDataType": "dom-checkbox" },
+            { "orderDataType": "dom-checkbox" }
+        ]
+
+    })
+};
+
+function initdatatable_add(elt) {
+    $(elt).DataTable({
+        "language": {
+            "lengthMenu": 'Afficher <select>'+
+            '<option value="10">10</option>'+
+            '<option value="25">25</option>'+
+            '<option value="50">50</option>'+
+            '<option value="100">100</option>'+
+            '<option value="-1">Tous</option>'+
+            '</select> éléments',
+            "zeroRecords": "Aucune entrée trouvée",
+            "info": "Affichage éléments _START_ à _END_ sur _TOTAL_ éléments",
+            "infoEmpty": "Pas d'élément disponible",
+            "search":         "Recherche ",
+            "infoFiltered":   "(filtrage sur un total de _MAX_ éléments)",
+            "paginate": {
+                "first":      "Premier",
+                "last":       "Dernier",
+                "next":       "Suivant",
+                "previous":   "Précédent"
+            }
+        },
+        "columns": [
+            null, null,
+            { "orderDataType": "dom-checkbox" },
+            { "orderDataType": "dom-checkbox" }
+        ]
+
+    })
+};
