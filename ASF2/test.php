2a570eeb5d1c43a567e7b79603f05158bcaa8173
peggy_fernandez <peggy.fernandez-blanco@univ-amu.fr>
2016-07-12 09:59:35 +0200

    Modif css AMU

--- ASF2/test.php
+++ ASF2/test.php
@@ -1,7 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: peggy_fernandez
- * Date: 10/05/2016
- * Time: 09:18
- */
+
+// La séquence de base avec LDAP est
+// connexion, liaison, recherche, interprétation du résultat
+// déconnexion
+
+echo '<h3>requête de test de LDAP</h3>';
+echo 'Connexion ...';
+$ds=ldap_connect("ldap1.univ-amu.fr", 389);  // doit être un serveur LDAP valide !
+echo 'Le résultat de connexion est ' . $ds . '<br />';
+
+if ($ds) {
+    echo 'Liaison ...';
+    $r=ldap_bind($ds, "cn=ipro,ou=system,dc=univ-amu,dc=fr", "c1pro2stag$");     // connexion anonyme, typique
+    echo 'Le résultat de connexion est ' . $r . '<br />';
+
+    // Recherche b007
+    $sr=ldap_search($ds, "dc=univ-amu,dc=fr", "uid=b007");
+    echo 'Le résultat de la recherche est ' . $sr . '<br />';
+
+    echo 'Le nombre d\'entrées retournées est ' . ldap_count_entries($ds,$sr)
+        . '<br />';
+
+    echo 'Lecture des entrées ...<br />';
+    $info = ldap_get_entries($ds, $sr);
+    echo 'Données pour ' . $info["count"] . ' entrées:<br />';
+
+    for ($i=0; $i<$info["count"]; $i++) {
+        echo 'dn est : ' . $info[$i]["dn"] . '<br />';
+        echo 'premiere entree cn : ' . $info[$i]["cn"][0] . '<br />';
+        echo 'premier email : ' . $info[$i]["mail"][0] . '<br />';
+    }
+
+    echo 'Fermeture de la connexion';
+    ldap_close($ds);
+
+} else {
+    echo '<h4>Impossible de se connecter au serveur LDAP.</h4>';
+}
+?>
